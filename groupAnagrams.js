4ec23abfa7d8a89b9de4d77d238f043e8b3c2a27
Thanh Pham <jonathanphamw22@gmail.com>
2024-11-14 12:14:40 -0800

    group ananagram solution in javascript

---
+++ groupAnagrams.js
@@ -0,0 +1,19 @@
+const groupAnagrams = (strs) => {
+    let groups = {}
+
+   for( let str of strs) {
+    let key  = str.split('').sort().join('');
+    if(!groups[key]){
+        groups[key] = []
+    }
+    groups[key].push(str) // push original string into right group array
+
+
+   }
+   return Object.values(groups)
+}
+
+let example = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+result = groupAnagrams(example)
+console.log(result)
