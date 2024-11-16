38e92a2d823d9655fa319e62f1582fa7cd3bc3f2
Thanh Pham <jonathanphamw22@gmail.com>
2024-11-15 22:48:20 -0800

    finished sudoku problems

--- isValidSudoku.py
+++ isValidSudoku.py
@@ -5,4 +5,25 @@ def isValidSoduku(board):
     row =defaultdict(set)
     col = defaultdict(set)
     box = defaultdict(set)
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            current_value = board[r][c]
+            box_coordinate = f'{r//3}, {c//3}' # coordinate for sub-box inside board 3x3 keeping track of each value inside sub-box
+            if current_value != ".":
+                if current_value in row[r] or current_value in col[c] or current_value in box[box_coordinate]:
+                    return False
+                row[r].add(current_value)
+                col[c].add(current_value)
+                box[box_coordinate].add(current_value)
+    return True
     
+board1 = [["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+print(isValidSoduku(board1))
