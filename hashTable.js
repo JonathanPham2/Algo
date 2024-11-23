a331ef7f05787e38004d72edb45c7ff23d804a52
Thanh Pham <jonathanphamw22@gmail.com>
2024-11-22 23:23:26 -0800

    definition of hash

--- hashTable.js
+++ hashTable.js
@@ -1,2 +1,3 @@
 // hash function is any function that can be used to map data of abitrary size to data of a fixed sized .
+// the value returned by a hash function called hash value, hash codes, digest, or simply hash
 
