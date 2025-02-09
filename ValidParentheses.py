0198b04e89303f57757da1770cfded44ef9e7fe8
Thanh Pham <jonathanphamw22@gmail.com>
2025-02-09 15:31:36 -0800

    Finished valid parenthese problem with comment explaining each line of code

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -31,26 +31,29 @@
 
 # Output: true
 
-# Approach:
-    # Using a stack and hash map
-    # Hash map store closing parenthese as a key and open parathenese as a value
-    # initialize a stack
-    # loop through the string
-    # Whenever we encounter a open  parentheses push it into the stack
-    # when we encounter a closing parenthese. We pop the parenthese at the top of out stack
-    # Compare it with using a hash map with the closing parenthese as a key if the value match we continue else return false
+# !Approach:
+    #?Using a stack and hash map
+    # ? Hash map store closing parenthese as a key and open parathenese as a value
+    #?initialize a stack
+    #?loop through the string
+    #?Whenever we encounter a open  parentheses push it into the stack
+    #?when we encounter a closing parenthese. We pop the parenthese at the top of out stack
+    # ?Compare it with using a hash map with the closing parenthese as a key if the value match we continue else return false
 class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) < 2:
-            return False
+            return False  #?First we check if the lenght of the string input is less than 2 
+            # ?meaning that this string is invalid in first place !!!
         m = {
             ")":"(",
             "}":"{",
             "]":"["
         }
-        stack = []
-        for b in s:
-            if b in m.values():
+        #?a dictionary containing closing brackets as a key and open brackes as value for easy retrieving and comparing late on in the code
+
+        stack = [] #? initalize a stack 
+        for b in s: #? loop throuhg the
+            if b in m.values(): #? Now we check if this string is an open bracket or not if yes push it into the stack
                 stack.append(b)
             else:
                 if len(stack) == 0:
